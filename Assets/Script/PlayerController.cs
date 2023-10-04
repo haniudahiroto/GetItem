a3595b7d97a8b8b4538adc4ac422aa83f96534f4
haniudahiroto <hara.jiburarutal@gmail.com>
2023-10-04 16:22:03 +0900

    コメントアウトの追加マテリアルも

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,28 +2,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // キャラクターの移動速度
     public float moveSpeed = 5f;
+
+    // 近くのアイテムを格納する変数
     private GameObject nearbyItem;
+
+    // アイテムを拾えるかどうかを示すフラグ
     private bool canPickUp = false;
 
     void Update()
     {
+        // プレイヤーの入力を受け取る
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        // 移動ベクトルを計算し、キャラクターを移動させる
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
         transform.Translate(movement);
 
+        // アイテムを拾う条件をチェック
         if (canPickUp && Input.GetKeyDown(KeyCode.E) && nearbyItem != null)
         {
+            // アイテムを拾う処理を実行
             PickUpItem();
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // 他のコライダーとの衝突時に呼ばれる
         if (other.CompareTag("Item"))
         {
+            // 近くにアイテムがある場合、それを格納し、アイテムを拾える状態にする
             nearbyItem = other.gameObject;
             canPickUp = true;
         }
@@ -31,8 +42,10 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        // 衝突から離れたときに呼ばれる
         if (other.CompareTag("Item"))
         {
+            // アイテムから離れたので、近くのアイテムをクリアし、アイテムを拾えない状態に戻す
             nearbyItem = null;
             canPickUp = false;
         }
@@ -42,6 +55,8 @@ public class PlayerController : MonoBehaviour
     {
         // アイテムの取得処理をここに記述
         Debug.Log("アイテムを取得しました: " + nearbyItem.name);
+
+        // アイテムを削除し、関連する変数をリセット
         Destroy(nearbyItem);
         nearbyItem = null;
         canPickUp = false;
